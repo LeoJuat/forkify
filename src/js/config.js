fbd9dd845777333392f9259e77b9533f73038553
leojuat <leonellejuat@gmail.com>
2022-05-27 14:50:01 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 11;
+export const KEY = '43acc140-9982-4c8e-8f06-1dfee576e785';
+export const MODAL_CLOSE_SEC = 2.5;
